da60a7c25501c37891e5be62248aeeba4f8b6033
yechanJung <ychung@idrsoft.com>
2022-09-05 15:16:43 +0900

    2022.09.05 ycjung 병렬처리 테스트 로직 추가

--- CommonUtil/src/com/ycjung/parallel/CorePerformance.java
+++ CommonUtil/src/com/ycjung/parallel/CorePerformance.java
@@ -10,7 +10,7 @@ public class CorePerformance {
 
     private static final Logger log = LoggerFactory.getLogger(CorePerformance.class);
 
-    public static void main(String[] args) {
+    public static void case1() {
         System.out.println("stream (16 elements) ");
 
         final StopWatch stopWatch = new StopWatch();
@@ -26,5 +26,28 @@ public class CorePerformance {
         .forEach(i -> log.info("i = {}", i));
         stopWatch.stop();
         log.info(">>> stream takes [{}]ms", stopWatch.getTotalTimeMillis());
+    }
+
+    public static void case2() {
+        log.info("==== parallel stream (16 elements) ====");
+        final StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+        IntStream.rangeClosed(1, 16)
+            .parallel()
+            .peek(i -> {
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            })
+            .forEach(i -> log.info("i = {}", i));
+        stopWatch.stop();
+        log.info(">>> parallel stream takes [{}]ms", stopWatch.getTotalTimeMillis());
+    }
+
+    public static void main(String[] args) {
+        //case1();
+        case2();
     }
 }
